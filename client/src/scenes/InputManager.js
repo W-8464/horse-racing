e279837ad7fbe6d052941dcfbaa827824e23c971
QE <htc@TRAMLABEL459.vnfgte.foxconn.com>
2026-02-04 10:34:12 +0700

    update v2.9.1_2

--- client/src/scenes/InputManager.js
+++ client/src/scenes/InputManager.js
@@ -8,10 +8,20 @@ export default class InputManager {
     }
 
     init() {
-        this.scene.input.on('pointerdown', (pointer) => this.onPointerDown(pointer));
+        // this.scene.input.on('pointerdown', (pointer) => this.onPointerDown(pointer));
+
+        this.scene.input.on('pointerdown', (pointer) => {
+            pointer.preventDefault()
+            pointer.stopPropagation()
+            return this.onPointerDown(pointer)
+        });
+
+
+
     }
 
     onPointerDown(pointer) {
+
         if (this.ui.isWinnerOpen()) return;
         if (this.state.role !== 'player') return;
 
